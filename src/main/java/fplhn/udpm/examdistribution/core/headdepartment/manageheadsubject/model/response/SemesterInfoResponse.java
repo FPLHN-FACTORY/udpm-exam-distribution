bf43fbc6c61ceee4637de28acca17a17cec3dcdf
manhnv99 <nguyenvimanhnqt@gmail.com>
2024-08-01 01:42:23 +0700

    remove subject and subject group and remake db

--- src/main/java/fplhn/udpm/examdistribution/core/headdepartment/manageheadsubject/model/response/SemesterInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package fplhn.udpm.examdistribution.core.headdepartment.manageheadsubject.model.response;
-
-import fplhn.udpm.examdistribution.core.common.base.IsIdentify;
-
-public interface SemesterInfoResponse extends IsIdentify {
-
-    String getSemesterInfo();
-
-}
--- src/main/java/fplhn/udpm/examdistribution/core/headdepartment/manageheadsubject/repository/HDDepartmentFacilityExtendRepository.java
+++
@@ -1,8 +0,0 @@
-package fplhn.udpm.examdistribution.core.headdepartment.manageheadsubject.repository;
-
-import fplhn.udpm.examdistribution.repository.DepartmentFacilityRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HDDepartmentFacilityExtendRepository extends DepartmentFacilityRepository {
-}
